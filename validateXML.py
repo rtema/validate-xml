e965d246694191ce269d3fd20cc2d1098e22a1cd
Moritz1404 <moritzsteinbusch@gmail.com>
2022-07-07 09:40:45 +0200

    validateXML.py hinzugefügt

---
+++ validateXML.py
@@ -0,0 +1,19 @@
+from lxml import etree
+
+
+def validate(xml_path: str, xsd_path: str) -> bool:
+
+    xmlschema_doc = etree.parse(xsd_path)
+    xmlschema = etree.XMLSchema(xmlschema_doc)
+
+    xml_doc = etree.parse(xml_path)
+    result = xmlschema.validate(xml_doc)
+
+    return result
+
+
+if validate("./company-02/ddf-example-controller.xml", "./schema.xsd"):
+    print("Valid!")
+else:
+    print("Not vaild!")
+    
